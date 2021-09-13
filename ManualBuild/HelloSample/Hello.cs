cb63764311ff80277b757787cd4087a3ac57146d
guigui55 <guigui55@outlook.jp>
2021-09-14 07:57:12 +0900

    210914-0757

---
+++ ManualBuild/HelloSample/Hello.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Hello{
+  public static void Main(){
+    Console.WriteLine("hello world!");
+    Console.WriteLine(new TestClass().GetClassName());
+    Console.ReadKey();
+  }
+}
---
+++ ManualBuild/HelloSample/TestClass.cs
@@ -0,0 +1,6 @@
+using System;
+
+public class TestClass{
+  public TestClass(){  }
+  public string GetClassName(){ return this.ToString(); }
+}
