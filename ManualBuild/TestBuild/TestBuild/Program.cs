d267e9018943bdbaebc819813031117c7f6c9a8d
guigui55 <guigui55@outlook.jp>
2021-09-16 00:39:38 +0900

    Add Doc ManualBuild

---
+++ ManualBuild/TestBuild/TestBuild/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project For TestBuild");
+            Console.ReadKey();
+
+        }
+    }
+}
