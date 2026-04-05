c9eca3e1860185f759c9803eb23963f869e1eb77
guigui55-ok <utauinu8102@nifty.com>
2026-04-06 00:22:03 +0900

    260406 0021

---
+++ InstallShield/TestWinforms1/TestWinforms1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinforms1
+{
+    public partial class FormTestWInforms1 : Form
+    {
+        public FormTestWInforms1()
+        {
+            InitializeComponent();
+        }
+    }
+}
